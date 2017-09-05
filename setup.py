43bb9af55f831327450b25e68199e8e4a3c60aa0
M Anzuoni <michaelan@kinja.com>
2017-09-05 12:26:36 -0400

    Up

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'dnsdiff',
   packages = ['dnsdiff'], # this must be the same as the name above
-  scripts=['dnsdiff/dnsdiff'],
-  version = '0.5',
+  scripts=['dnsdiff/dnsdiff.py'],
+  version = '0.6',
   description = 'Utility to quickly suss out discrepancies between nameservers',
   author = 'M Anzuoni',
   author_email = 'me.anzuoni@gmail.com',
   url = 'https://github.com/gawkermedia/dnsdiff', # use the URL to the github repo
-  download_url = 'https://github.com/gawkermedia/dnsdiff/archive/0.5.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/gawkermedia/dnsdiff/archive/0.6.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'dns',], # arbitrary keywords
   classifiers = [],
   install_requires=['dnspython']
