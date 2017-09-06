260d78d6cebaacbefdf130f991bdfdfada834e03
M Anzuoni <michaelan@kinja.com>
2017-09-06 10:23:33 -0400

    Guess PIP didn't like that

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'M Anzuoni',
   author_email = 'me.anzuoni@gmail.com',
   url = 'https://github.com/gawkermedia/dnsdiff', # use the URL to the github repo
-  download_url = 'https://github.com/gawkermedia/dnsdiff/archive/1.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/gawkermedia/dnsdiff/archive/1.2.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'dns',], # arbitrary keywords
   classifiers = [],
   install_requires=['dnspython']
